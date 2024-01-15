296e0b004633d6ef4c3de64b5b37ef056e9b4277
Kieran Boyle <norcal.kieran@gmail.com>
2024-01-15 09:13:17 -0800

    fix(component): adjusting case for component

---
+++ src/Component.snap.js
@@ -0,0 +1,32 @@
+// Vitest Snapshot v1, https://vitest.dev/guide/snapshot.html
+
+exports[`Component > should render the component 1`] = `
+<body>
+  <div>
+    <button
+      class="component svelte-1eibeh7"
+    >
+      Empty
+    </button>
+    <!--&lt;Component&gt;-->
+    
+    <!--&lt;Component.test&gt;-->
+  </div>
+</body>
+`;
+
+exports[`Component > should render the correct content value 1`] = `
+<body>
+  <div>
+    <button
+      class="component svelte-1eibeh7"
+    >
+      Mahalo!
+      
+    </button>
+    <!--&lt;Component&gt;-->
+    
+    <!--&lt;Component.test&gt;-->
+  </div>
+</body>
+`;
---
+++ src/Component.svelte
@@ -0,0 +1,78 @@
+<script>
+  // @ts-check
+  /**
+   * The text to display in the default slot.
+   *
+   * @type {string}
+   * @default 'Empty'
+   */
+  export let placeholder = 'Empty';
+
+  /**
+   * The disabled state of the component.
+   *
+   * @type {boolean}
+   * @default false
+   */
+  export let disabled = false;
+</script>
+
+<button class="component" on:click {disabled} {...$$restProps}>
+  <slot>{placeholder}</slot>
+</button>
+
+<style>
+  :root {
+    --component-background: #fff;
+    --component-background-active: #ddd;
+    --component-background-disabled: #ccc;
+    --component-background-hover: #eaeaea;
+
+    --component-border: 1px solid #eee;
+    --component-border-active: 1px solid #aaa;
+    --component-border-active: 1px solid #bbb;
+    --component-border-hover: 1px solid #ccc;
+    --component-border-radius: 4px;
+
+    --component-color: #111;
+    --component-color-active: #333;
+    --component-color-disabled: #999;
+    --component-color-hover: #777;
+
+    --component-cursor: pointer;
+    --component-cursor-disabled: default;
+
+    --component-display: inline-block;
+    --component-padding: 12px 16px;
+  }
+
+  .component {
+    appearance: none;
+    background: var(--component-background);
+    border: var(--component-border);
+    border-radius: var(--component-border-radius);
+    color: var(--component-color);
+    cursor: var(--component-cursor);
+    display: var(--component-display);
+    padding: var(--component-padding);
+  }
+
+  .component:hover {
+    background: var(--component-background-hover);
+    border: var(--component-border-hover);
+    color: var(--component-color-hover);
+  }
+
+  .component:active {
+    background: var(--component-background-active);
+    border: var(--component-border-active);
+    color: var(--component-color-active);
+  }
+
+  .component[disabled] {
+    background: var(--component-background-disabled);
+    border: var(--component-border-disabled);
+    color: var(--component-color-disabled);
+    cursor: var(--component-cursor-disabled);
+  }
+</style>
